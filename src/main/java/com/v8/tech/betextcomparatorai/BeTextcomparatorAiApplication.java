5c1da8160c019a0d7b23fb94e02d57d47f71ec11
Dev001 B <dev001-b@v8techlabs.local>
2024-05-10 15:24:52 -0300

    commit inicial - inicio do projeto

---
+++ src/main/java/com/v8/tech/betextcomparatorai/BeTextcomparatorAiApplication.java
@@ -0,0 +1,13 @@
+package com.v8.tech.betextcomparatorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTextcomparatorAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTextcomparatorAiApplication.class, args);
+	}
+
+}
